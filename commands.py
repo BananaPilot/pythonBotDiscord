1f48d91e9d2f917bd31879338f6154a226ad12f5
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-22 20:00:44 +0100

    changed ctx to context to check the webhook

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 from bot_init import bot
 
 @bot.command(name='hello', description="Greet the user!")
-async def hello(ctx, args):
-	await ctx.send(f"Hello {ctx.author.name}!")
+async def hello(context, args):
+	await context.send(f"Hello {context.author.name}!")
