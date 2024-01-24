df0759b2ddc7a97718aa28b589446d2099654801
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-24 16:45:41 +0100

    gay 2

--- bot.sh
+++ bot.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 check_apt_package() {
-  if ! dpkg -s $1 >/dev/null 2>&1; then
-    echo $1 not found, installing...
-    sudo apt install $1
-  else
-    echo $1 is already installed
-  fi
+	if ! dpkg -s $1 >/dev/null 2>&1; then
+		echo $1 not found, installing...
+		sudo apt install $1
+	else
+		echo $1 is already installed
+	fi
 }
 
-check_python_package() {
-	if ! pip show $1 >/dev/null 2>&1; then
-		echo $1 not found, installing...
-		pip install $1
-	fi
-#   else
-#     echo $1 is already installed
-#   fi
-}
+# check_python_package() {
+# 	if ! pip show $1 >/dev/null 2>&1; then
+# 		echo $1 not found, installing...
+# 		pip install $1
+# 	fi
+# #   else
+# #     echo $1 is already installed
+# #   fi
+# }
 
 echo checking dependencies...
 
 while read package; do
-  check_apt_package $package
+	check_apt_package $package
 done < dependencies.txt
 
 echo creating .venv for python...
@@ -35,22 +35,28 @@ echo virtual environment created.
 
 echo checking python requirements...
 
-while read package; do
-  check_python_package $package
-done < requirements.txt
+# while read package; do
+# 	check_python_package $package
+# done < requirements.txt
+pip install -r requirements.txt
 
 echo dependencies installed.
 
+deactivate # exit virtual environment
+
 echo checking for screen...
 
+process_name="discord_bot"
 if [ "$TERM" = "screen" ]; then
-  echo running the script...
-  python main.py
+	echo user is already in a screen.
+	echo running the script...
+	python main.py
 else
-  session_number=1
-  while screen -list | grep -q "discord_bot_$session_number"; do
-    session_number=$((session_number+1))
-  done
-  echo running the script...
-  screen -dmS discord_bot_$session_number python main.py
+	echo creating screen...
+	session_number=1
+	while screen -list | grep -q "${process_name}_$session_number"; do
+	session_number=$((session_number+1))
+	done
+	echo running the script...
+	screen -dmS ${process_name}_$session_number python main.py
 fi
